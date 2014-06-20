784a36b5e3e1554b4fc441c45f3704e36fa38f4c
Josh <firstmilkman@gmail.com>
2014-06-20 10:54:59 -0400

    Moved documentation from commented code to readme

--- README.md
+++ README.md
@@ -1,4 +1,129 @@
 extend.js
 =========
-
 A library that extends base JavaScript functionality
+
+
+Documentation:
+
+String
+	
+	String.startsWith
+		Description: Checks to see if the selected string starts with the given input.
+		Usage: String.startsWith(input)
+		Return Type: boolean
+		Usage Examples: 
+			"test".startsWith("te"); 
+				Returns true
+			"test".startsWith("st"); 
+				Returns false
+			"test".startsWith("tex"); 
+				Returns false
+
+	String.endsWith
+		Description: Checks to see if the selected string ends with the given input.
+		Usage: String.endsWith(input)
+		Return Type: boolean
+		Usage Examples: 
+			"test".endsWith("te"); 
+				Returns false
+			"test".endsWith("st"); 
+				Returns true
+			"test".endsWith("tex"); 
+				Returns false
+				
+	String.contains
+		Description: Checks to see if the selected string contains the given input.
+		Usage: String.contains(input)
+		Return Type: boolean
+		Usage Examples:
+			"test".contains('te');
+				Returns true
+			"test".contains('tex');
+				Returns false
+				
+	String.padLeft
+		Description: Pads a string to a set amount of character using defined characters
+		Usage: String.padLeft(character[s], integer)
+		Return Type: string
+		Usage Examples:
+			"string".padLeft('0', 15);
+				Returns "000000000string"
+			"string".padLeft('0a', 15);
+				Returns "0a0a0a0a0string"
+				
+	String.padRight
+		Description: Pads a string to a set amount of character using defined characters
+		Usage: String.padRight(character[s], integer)
+		Return Type: string
+		Usage Examples:
+			"string".padRight('0', 15);
+				Returns "string000000000"
+			"string".padRight('0a', 15);
+				Returns "string0a0a0a0a0"
+				
+	String.replaceChar
+		Description: Allows replacement of a character at the specified index in a string
+		Usage: String.replaceChar(char, index)
+		Return Type: string
+		Usage Example:
+			"texting".replaceChar('s', 2); 
+				Returns "testing"
+				
+	String.replaceChars
+		Description: Allows replacement of a specified number of character from a specified index. Builds upon replaceChar()
+		Usage: String.replaceChars(char, startIndex, length) //length is inclusive of the starting index
+		Return Type: string
+		Usage Example:
+			"texting".replaceChars("+", 2, 3);
+				Returns 'te+++ng'
+				
+	String.indicesOf
+		Description: Finds all occurrences of a specified string within another string. Builds upon indexOf()
+		Usage: String.indicesOf(searchString)
+		Return Type: object (Array)
+		Usage Example:
+		'texting'.indicesOf('t'); 
+			Returns [0, 3]
+
+Number
+	
+	isNumeric
+		Description: Checks to see if the input is a number.
+		Usage: isNumeric(input); 
+		Return Type: boolean
+		Usage Example: 
+		isNumeric(12); 
+			Returns true
+		isNumeric("12"); 
+			Returns true (due to automatic casting)
+		isNumeric("test"); 
+			Returns false
+			
+Array
+
+	Array.contains
+		Description: Checks to see if the selected array contains the given input.
+		Usage: [].contains(input); 
+		Return Type: boolean
+		Usage Examples: 
+			[1,2,3,4,5].contains('te'); 
+				Returns false
+			[1,2,3,4,5].contains(5); 
+				Returns true
+			[1,2,3,4,5].contains('5'); 
+				Returns true (due to automatic casting)
+			
+Function
+
+	Function.perfTest
+		Description: Allows for easy performance testing of functions returning time taken in milliseconds (less is better) or the amount of executions per second (more is better). The specified function can ran multiple times for performance testing smaller functions. You can also optionally pass the object the function is to be called on.
+		Usage: Function.perfTest(timesToExecute, executionObject, returnExecutionsPerSecond, arg1, arg2, arg3...)
+		Return Type: number
+		Usage Examples:
+			var myFunc = function(input1, input2);
+			myFunc.perfTest(1, null, false, "input1", "input2"); //will run once and display the time it took in ms
+			myFunc.perfTest(10, null, true, "input1", "input2"); //will run 100 times and display how many times it can run per second.
+			
+			var myArray = [];
+			myArray.push.perfTest(10, myArray, true, "value"); //will insert the string "value" 10 times into "myArray" and display how many times it can run per second.
+			[].push.perfTest(10, myArray, true, "value"); //will have the same result as the code on the line above.
--- extend.js
+++ extend.js
@@ -1,12 +1,4 @@
-﻿// A class for extending JS functionality
-
-//Name: padLeft
-//Description: Pads a string to a set amount of character using defined characters
-//Usage: String.padLeft(character[s], integer)
-//Usage Examples:
-//"string".padLeft('0', 15);
-//"string".padLeft('0a', 15);
-String.prototype.padLeft = function (char, length) {
+﻿String.prototype.padLeft = function (char, length) {
     if (typeof char === "string" && typeof length === "number" && length >= 1) {
         var tempString = "";
         var loops = Math.floor((length - this.length) / char.length);
@@ -23,12 +15,6 @@ String.prototype.padLeft = function (char, length) {
     }
 };
 
-//Name: padRight
-//Description: Pads a string to a set amount of character using defined characters
-//Usage: String.padRight(character[s], integer)
-//Usage Examples:
-//"string".padRight('0', 15);
-//"string".padRight('0a', 15);
 String.prototype.padRight = function (char, length) {
     if (typeof char === "string" && typeof length === "number" && length >= 1) {
         var tempString = "";
@@ -46,16 +32,6 @@ String.prototype.padRight = function (char, length) {
     }
 };
 
-//Name: perfTest
-//Description: Allows for easy performance testing of functions returning time taken in milliseconds (less is better) or the amount of executions per second (more is better). The specified function can ran multiple times for performance testing smaller functions. You can also optionally pass the object the function is to be called on.
-//Usage: Function.perfTest(timesToExecute, executionObject, returnExecutionsPerSecond, arg1, arg2, arg3...)
-//Usage Example:
-//var myFunc = function(input1, input2);
-//myFunc.perfTest(1, null, false, "input1", "input2"); //will run once and display the time it took in ms
-//myFunc.perfTest(10, null, true, "input1", "input2"); //will run 100 times and display how many times it can run per second.
-//var myArray = [];
-//myArray.push.perfTest(10, myArray, true, "value"); //will insert the string "value" 10 times into "myArray" and display how many times it can run per second.
-//[].push.perfTest(10, myArray, true, "value"); //will have the same result as the code on the line above.
 Function.prototype.perfTest = function (loops, obj, returnExecutionsPerSecond) {
     var args = [].splice.call(arguments, 3);
     var start = new Date();
@@ -71,36 +47,20 @@ Function.prototype.perfTest = function (loops, obj, returnExecutionsPerSecond) {
     }
 };
 
-//Name: isNumeric
-//Description: Checks to see if the input is a number.
-//Usage: isNumeric(input); //returns true.
-//Usage Example: 
-//isNumeric(12); //returns true.
-//isNumeric("12"); //will also return true due to automatic casting.
-//isNumeric("test"); //returns false.
 var isNumeric = function (number) {
     return (!isNaN(number) && isFinite(number));
 }
 
-//Name: replaceChar
-//Description: Allows replacement of a character at the specified index in a string
-//Usage: String.replaceChar(char, index)
-//Usage Example:
-//'texting'.replaceChar('s', 2); //will result in 'testing'
 String.prototype.replaceChar = function(char, index) {
     if (index >= 0 && index < this.length) {
         var str = this.split("");
+		char.length > 1 ? char = char.charAt(0) : null;
         str[index] = char;
         return str.join("");
     }
     return this;
 }
 
-//Name: replaceChars
-//Description: Allows replacement of a specified number of character from a specified index. Builds upon replaceChar()
-//Usage: String.replaceChars(char, startIndex, length) // length is inclusive of the starting index
-//Usage Example:
-//'texting'.replaceChars('+', 2, 3); //will result in 'te+++ng'
 String.prototype.replaceChars = function (char, startIndex, length) {
     if (startIndex >= 0 && length >= 0 && startIndex < this.length) {
         startIndex + length <= this.length ? null : length = this.length - startIndex;
@@ -113,11 +73,6 @@ String.prototype.replaceChars = function (char, startIndex, length) {
     return this;
 }
 
-//Name: indicesOf
-//Description: Finds all occurrences of a specified string within another string. Builds upon indexOf()
-//Usage: String.indicesOf(searchString)
-//Usage Example:
-//'texting'.indicesOf('t'); //will result in [0, 3]
 String.prototype.indicesOf = function (searchString) {
     var startPoint = 0;
     var result = -1;
@@ -129,35 +84,18 @@ String.prototype.indicesOf = function (searchString) {
     return indices;
 }
 
-//Name: startsWith
-//Description: Checks to see if the selected string starts with the given input.
-//Usage: String.startsWith(input); //returns bool.
-//Usage Example: 
-//'test'.startsWith('te'); //returns true.
-//'test'.startsWith('st'); //returns false.
-//'test'.startsWith('tex'); //returns false.
 String.prototype.startsWith = function (input) {
     return this.indexOf(input) == 0;
 };
 
-//Name: endsWith
-//Description: Checks to see if the selected string ends with the given input.
-//Usage: String.endsWith(input); //returns bool.
-//Usage Example: 
-//'test'.endsWith('te'); //returns false.
-//'test'.endsWith('st'); //returns true.
-//'test'.endsWith('tex'); //returns false.
 String.prototype.endsWith = function (input) {
     return this.indexOf(input) == this.length - input.length;
 };
 
-//Name: contains
-//Description: Checks to see if the selected array contains the given input.
-//Usage: [].contains(input); //returns bool.
-//Usage Example: 
-//[1,2,3,4,5].contains('te'); //returns false.
-//[1,2,3,4,5].contains(5); //returns true.
-//[1,2,3,4,5].contains('5'); //returns true due to automatic casting.
+String.prototype.contains = function(input) {
+	return (this.indexOf(input) !== -1);
+};
+
 Array.prototype.contains = function (input) {
     for (var i = 0; i < this.length; i++) {
         if (this[i] == input) {
